68b34cf8ca03d6ddcc5f01efc94616fc84e6d1af
mathewselvi <mathewselvi29@gmail.com>
2026-05-27 11:47:01 +0530

    smallchanges

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,17 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://kvsdashboard-cbvk.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.use(express.json());
 
 // Routes
